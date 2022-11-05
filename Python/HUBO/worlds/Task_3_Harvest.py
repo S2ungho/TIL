c709c2cf86ed2d10ba8018e92fec5cf63e8ca73e
seungho1206 <bc1206@naver.com>
2022-11-05 22:24:17 +0900

    task3

--- Python/HUBO/worlds/Task_3_Harvest.py
+++ Python/HUBO/worlds/Task_3_Harvest.py
@@ -3,7 +3,6 @@ from cs1robots import*
 load_world("Python/HUBO/worlds/harvest1.wld")
 hubo = Robot() 
 hubo.set_trace('blue')
-
 # Write your own code below !
 def turn_right():
     for i in range(3):
@@ -11,15 +10,25 @@ def turn_right():
 
 #def find_beeper():
 
-
 def move_and_pick():
+    cnt = 0
     hubo.move()
     if hubo.on_beeper():
         hubo.pick_beeper()
+        cnt += 1
+
+cnt = 0
+while cnt < 36:
+    if hubo.front_is_clear():
+        move_and_pick()
+    else:
+        hubo.turn_left()
+
 
 
 
 #hubo.front_is_clear()
 #hubo.pick_beeper()
+
 #hubo.on_beeper()
 
