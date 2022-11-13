7e1ede8e329064c8e5aedd53a13c2954f56d2cf2
seungho1206 <bc1206@naver.com>
2022-11-13 17:02:08 +0900

    javaLab

---
+++ Java/ShapeTest.java
@@ -0,0 +1,49 @@
+interface Area{
+    void getArea();
+}
+interface Movable extends Area{
+    void move(int dx, int dy);
+}
+class Shape implements Movable{
+    int x, y;
+    public void move(int dx, int dy){
+        x += dx;
+        y += dy;
+    }
+    public void getArea(){
+        System.out.println(x + " " + y);
+    }
+}
+class Rectangle extends Shape{
+    int weight, height;
+    public void getArea(){
+        System.out.println(x + " " + y);
+    }
+}
+class Triangle extends Shape{
+    int base, height;
+    public void getArea(){
+        System.out.println(x + " " + y);
+    }
+}
+class Circle extends Shape{
+    int radius;
+    public void getArea(){
+        System.out.println(x + " " + y);
+    }
+}
+
+public class ShapeTest {
+    public static void main(String args[]) {
+        Movable[] arrayOfShapes;
+        arrayOfShapes = new Movable[3];
+        arrayOfShapes[0] = new Rectangle();
+        arrayOfShapes[1] = new Triangle();
+        arrayOfShapes[2] = new Circle();
+        for(int i = 0;i < 3;i++){
+            arrayOfShapes[i] = new Shape();
+            arrayOfShapes[i].move(10,10);
+            arrayOfShapes[i].getArea();
+        }
+    }
+}
