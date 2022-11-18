d04a21b1f88859ac0e97e3317c73353501c83c01
seungho1206 <bc1206@naver.com>
2022-11-18 17:10:04 +0900

    1118

--- Python/HUBO/worlds/Task3_3_trash2.py
+++ Python/HUBO/worlds/Task3_3_trash2.py
@@ -1,55 +1,34 @@
 import time
 from cs1robots import *
-#create_world()
+#create_world(streets=10, avenues=10)
 load_world("Python/HUBO/worlds/yardwork.wld")
 hubo = Robot() 
 hubo.set_trace('blue')
 
-# Write your own code below !
 def turn_right():
-    for i in range(3):
-        Robot.turn_left()
+  for _ in range(3):
+    hubo.turn_left()
+  
+def move_straight():
+  while hubo.front_is_clear():
+    move_and_pick()
 
 def move_and_pick():
     hubo.move()
     while hubo.on_beeper():
         hubo.pick_beeper()
 
-def turn_right():
-    for i in range(3):
-        hubo.turn_left()
+def zig():
+  move_and_pick()
+  turn_right()
+  move_straight()
+  hubo.turn_left()
 
-def last_move():
-    while hubo.front_is_clear():
-        move_and_pick()
-
-
-def find_height():
-    while hubo.front_is_clear():
-        move_and_pick()
-        cnt += 1
-
-
-def Height():
-    global m, swi, n
-    last_move()
-    turn_right()
-    hubo.move()
-    turn_right()
-    last_move()
-    for i in range(m-1):
-        if not hubo.front_is_clear():
-            if n % 2 == 1:
-                hubo.turn_left()
-                move_and_pick()
-                hubo.turn_left()
-                n -= 1
-            else:
-                turn_right()
-                move_and_pick()
-                turn_right()
-                n -= 1
-            move_and_pick()
+def zag():
+  move_and_pick()
+  hubo.turn_left()
+  move_straight()
+  turn_right()
 
 def see():
     while not hubo.facing_north():
@@ -58,42 +37,22 @@ def see():
 
 def start_position():
     for i in range(2):
-        last_move()
+        move_straight()
         hubo.turn_left()
 
-def first_move():
-    global swi
-    last_move()
-    if not hubo.right_is_clear():
-        hubo.turn_left()
-        move_and_pick()
-        hubo.turn_left()
-        swi = 0
-        last_move()
-        turn_right()
-    else:
-        turn_right()
-        move_and_pick()
-        turn_right()
-        swi = 1
-        last_move()
-        hubo.turn_left()
-
-def zigzag():
-    global swi
-    if swi == 0:
-        move_and_pick()
-        turn_right()
-        swi = 1
-    else:
-        move_and_pick()
-        hubo.turn_left()
-        swi = 0
-    last_move()
-
-
-first_move()
-for i in range(8):
-    zigzag()
-#if hubo.front_is_clear():
-#    move_and_pick()
+c=0
+#hubo.turn_left()
+move_straight()
+#turn_right()
+hubo.turn_left()
+while hubo.front_is_clear():
+  if c==0:
+    zag()
+    c+=1
+  elif c==1:
+    zig()
+    c-=1
+  else:
+    print('error')
+see()
+start_position()
