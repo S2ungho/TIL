17044d8963d048aa45d446e4be39a990a2ff1333
seungho1206 <bc1206@naver.com>
2022-11-19 16:08:42 +0900

    1119

--- Python/HUBO/worlds/Task3_4_Rain.py
+++ Python/HUBO/worlds/Task3_4_Rain.py
@@ -1,59 +1,34 @@
 import time
 from cs1robots import *
-#create_world(streets=10, avenues=10)
-load_world("Python/HUBO/worlds/rain1.wld")
-hubo = Robot(beepers = 10, avenue = 3, street = 6, orientation = 'E')
+load_world("Python/HUBO/worlds/rain2.wld") #맵 크기 수정 rain2, 크기 22 -> 15
+hubo = Robot(beepers = 7, avenue = 3, street = 6, orientation = 'E')
 hubo.set_trace('blue')
 
 
 def turn_right():
   for _ in range(3):
     hubo.turn_left()
-  
-def move_straight():
-  while hubo.front_is_clear():
-    move_and_pick()
 
-def move_and_pick():
-    hubo.move()
-    while hubo.on_beeper():
-        hubo.pick_beeper()
-
-def zig():
-  move_and_pick()
-  turn_right()
-  move_straight()
-  hubo.turn_left()
-
-def zag():
-  move_and_pick()
-  hubo.turn_left()
-  move_straight()
-  turn_right()
-
-def see():
-    while not hubo.facing_north():
-        hubo.turn_left()
+def move_front():
+    while hubo.front_is_clear():
+      hubo.move()
+      time.sleep(0.2)
+      window()
     hubo.turn_left()
+    window()
 
-def start_position():
-    for i in range(2):
-        move_straight()
-        hubo.turn_left()
+def window():
+  if hubo.front_is_clear() and hubo.right_is_clear():
+    turn_right()
+    hubo.move()
+    if hubo.left_is_clear() and hubo.right_is_clear(): 
+      hubo.turn_left()
+      hubo.turn_left()
+      hubo.move()
+      hubo.drop_beeper()
+      turn_right()
 
-c=0
-#hubo.turn_left()
-move_straight()
-#turn_right()
-hubo.turn_left()
-while hubo.front_is_clear():
-  if c==0:
-    zag()
-    c+=1
-  elif c==1:
-    zig()
-    c-=1
-  else:
-    print('error')
-see()
-start_position()
+
+turn_right()
+while (1): #hubo.carries_beepers()
+  move_front()
