65a1bf741e36e5cd9b35a53504b08424e74efc68
seungho1206 <bc1206@naver.com>
2022-11-22 20:04:17 +0900

    1120

--- Python/HUBO/worlds/Task3_4_Rain.py
+++ Python/HUBO/worlds/Task3_4_Rain.py
@@ -1,7 +1,7 @@
 import time
 from cs1robots import *
 load_world("Python/HUBO/worlds/rain2.wld") #맵 크기 수정 rain2, 크기 22 -> 15
-hubo = Robot(beepers = 10, avenue = 3, street = 6, orientation = 'E')
+hubo = Robot(beepers = 7, avenue = 3, street = 6, orientation = 'E')
 hubo.set_trace('blue')
 
 def turn_right():
