67ce2a9ec519fa7fe9cdb2fed0cab2f47065483b
seungho1206 <bc1206@naver.com>
2022-11-22 19:44:07 +0900

    1120

---
+++ Python/HUBO/worlds/Task4_Animation.py
@@ -0,0 +1,12 @@
+from cs1graphics import*
+from time import*
+import copy
+
+def draw_animal():
+    print("a")
+
+paper = Canvas()
+paper.setBackgroundColor('Green')
+paper.setWidth(700)
+paper.setHeight(500)
+paper.setTitle('My World')
